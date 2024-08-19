0b5b7b93add3aebbfe94fe527d0c2cd5257ca55c
THIAGO MAXIMO SANTOS DA SILVA <thiagomaximo271@gmail.com>
2024-08-19 11:51:32 -0300

    Inserindo algumas classes de banco de dados

--- SysPecNSLib/Banco.cs
+++ SysPecNSLib/Banco.cs
@@ -3,10 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using MySql.Data.MySqlClient;
 namespace SysPecNSLib
 {
-    public class Banco
+    /// <summary>
+    /// Classe estatica (Não precisa de declaração de instancia) de Conexão de Banco de Dados
+    /// </summary>
+    public static class Banco //Classe de conexão de banco de dados
+    
     {
+        /// <summary>
+        /// Método abrir da classe Banco utilizado para representar comnados sql
+        /// no servidor, cujo os dados foram informados na string de conexão (strcon)
+        /// </summary>
+        /// <returns>Entrega um objeto de comandos SQL, que serão exexutados na conexão informada</returns>
+        public static MySqlCommand Abrir() //método de conexão
+        {
+            //dados da conexão
+            string strconn = @"server=127.0.0.1;database=syspecdb;user=root;password";
+            MySqlConnection cn = new MySqlConnection(strconn);
+            MySqlCommand cmd = new();
+            try //tratamento de conexão
+            {
+                cn.Open();
+                cmd.Connection = cn;
+            }
+            catch (Exception) 
+            {
+                throw;
+            }
+            return cmd;
+            
+        }
     }
 }
