1f7d312a7dcb7c8232e9f391564f9b4f5cd29d7a
THIAGO MAXIMO SANTOS DA SILVA <thiagomaximo271@gmail.com>
2024-08-20 10:35:11 -0300

    Primeira parte da aula sobre propriedades c#

--- SysPecNSLib/Nivel.cs
+++ SysPecNSLib/Nivel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,48 @@ namespace SysPecNSLib
 {
     public class Nivel
     {
+        // iremos Ver atributos da Classe, propriedades, métodos construtores, métodos requisitos
+
+       
+
+        //propriedades
+            public int Id {  get; set; }
+            public string? Nome { get; set; }
+            public string? Sigla { get; set; }
+        //Métodos de Acesso (get e set)
+            //um jeito de criar um método de acesso: public int id { get => Id; set => Id = value; }
+
+        //métodos construtores
+        public Nivel() { }
+
+        public Nivel(string nome, string sigla) // Sobrecarga de de métodos, é preciso declarar métodos com parametros diferentes
+        {
+            Nome = nome;
+        }
+        //métodos requisitos
+        public void inserir() // método para inserir registro de nível na base de dados
+        {
+
+        }
+        public static Nivel ObterPorId(int id)
+        {
+            Nivel nivel = new();
+            //consulta do Banco e retornar o nível
+            return nivel;
+        }
+        public static List<Nivel>ObterLista()
+        {
+            List<Nivel> lista = new();
+            //consulta para retornar a lista de níveis
+            return lista;
+        }
+        public bool Atualizar()
+        {
+            return true;
+        }
+        public void ExcluirNivel(int id)
+        {
+
+        }
     }
 }
