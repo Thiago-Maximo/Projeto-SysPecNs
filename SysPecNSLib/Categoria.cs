ec3264c68fa4dc1080fc5e6a3d20b76dc735f16e
THIAGO MAXIMO SANTOS DA SILVA <thiagomaximo271@gmail.com>
2024-09-02 11:53:53 -0300

    criada as duas classes de produto e categoria

--- SysPecNSLib/Categoria.cs
+++ SysPecNSLib/Categoria.cs
@@ -3,11 +3,83 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
 
 namespace SysPecNSLib
 {
     public class Categoria
     {
+        public int Id { get; set; }
+        public string? Nome { get; set; }
+        public string? Sigla { get; set; }
 
+
+
+        public Categoria(int id, string? nome, string? sigla)
+        {
+            Id = id;
+            Nome = nome;
+            Sigla = sigla;
+        }
+        public Categoria(string? nome, string? sigla)
+        {
+            Nome = nome;
+            Sigla = sigla;
+        }
+        public Categoria()
+        {
+         
+        }
+        public void Inserir()
+        {
+            var cmd = Banco.Abrir();
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.CommandText = "sp_categoria_insert";
+            cmd.Parameters.AddWithValue("spnome",Nome);
+            cmd.Parameters.AddWithValue("spsigla",Sigla);
+            Id = Convert.ToInt32(cmd.ExecuteScalar());
+        }
+        public  static Categoria ObterPorId(int? Id)
+        {
+            Categoria categoria = new();
+            var cmd = Banco.Abrir();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = $"Select * from categorias where id = {Id}";
+            var dr = cmd.ExecuteReader();
+            if (dr.Read())
+            {
+                categoria =new(dr.GetInt32(0),dr.GetString(1),dr.GetString(2));
+            }
+            return categoria;
+        }
+        public static List<Categoria> ObterLista(int? Id)
+        {
+            List<Categoria> categorias = new();
+            var cmd = Banco.Abrir();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = $"Select * from categorias where id = {Id}";
+            var dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                categorias.Add(new(dr.GetInt32(0), dr.GetString(1), dr.GetString(2)));
+            }
+            return categorias;
+        }
+        public void Atualizar()
+        {
+            var cmd = Banco.Abrir();
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.CommandText = "sp_categoria_update";
+            cmd.Parameters.AddWithValue("spid",Id);
+            cmd.Parameters.AddWithValue("spnome",Nome);
+            cmd.Parameters.AddWithValue("spsigla",Sigla);
+            cmd.ExecuteNonQuery();
+        }
+        public void Deletar()
+        {
+            var cmd = Banco.Abrir();
+            cmd.CommandText = $"Delete from categorias where id = {Id} ";
+            cmd.ExecuteNonQuery();
+        }
     }
 }
