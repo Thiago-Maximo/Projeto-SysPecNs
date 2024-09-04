fcf60b6f280a14f74a62fa1f0fa684d8cb2a1a7a
THIAGO MAXIMO SANTOS DA SILVA <thiagomaximo271@gmail.com>
2024-09-04 11:59:03 -0300

    Criando a classe de fornecedores

--- SysPecNSLib/Fornecedor.cs
+++ SysPecNSLib/Fornecedor.cs
@@ -3,10 +3,93 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
 
 namespace SysPecNSLib
 {
     public class Fornecedor
     {
+        //Declaração das Variaveis
+        public int? Id { get; set; }
+        public string? Razao_Social { get; set; }
+        public string? Fantasia { get; set; }
+        public string? Cnpj { get; set; }
+        public string? Contato { get; set; }
+        public string? Telefone { get; set; }
+        public string? Email { get; set; }
+
+        //Métodos Construtores
+        public Fornecedor(int? id, string? razao_Social, string? fantasia, string? cnpj, string? contato, string? telefone, string? email)
+        {
+            Id = id;
+            Razao_Social = razao_Social;
+            Fantasia = fantasia;
+            Cnpj = cnpj;
+            Contato = contato;
+            Telefone = telefone;
+            Email = email;
+        }
+        public Fornecedor( string? razao_Social, string? fantasia, string? cnpj, string? contato, string? telefone, string? email)
+        {
+            Razao_Social = razao_Social;
+            Fantasia = fantasia;
+            Cnpj = cnpj;
+            Contato = contato;
+            Telefone = telefone;
+            Email = email;
+        }
+        public Fornecedor()
+        {
+          
+        }
+
+        //Criando as Funcionalidades
+        public void Inserir()
+        {
+            var cmd = Banco.Abrir();
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.CommandText = "sp_fornecedores_insert";
+            cmd.Parameters.AddWithValue("sprazao_social",Razao_Social);
+            cmd.Parameters.AddWithValue("spfantasia", Fantasia);
+            cmd.Parameters.AddWithValue("spcnpj", Cnpj);
+            cmd.Parameters.AddWithValue("spcontato", Contato);
+            cmd.Parameters.AddWithValue("sptelefone", Telefone);
+            cmd.Parameters.AddWithValue("spemail", Email);
+            Id = Convert.ToInt32((cmd.ExecuteScalar()));
+        }
+        public void Atualizar()
+        {
+            var cmd = Banco.Abrir();
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.CommandText = "sp_fornecedores_update";
+            cmd.Parameters.AddWithValue("sprazao_social", Razao_Social);
+            cmd.Parameters.AddWithValue("spfantasia", Fantasia );
+            cmd.Parameters.AddWithValue("spcnpj", Cnpj);
+            cmd.Parameters.AddWithValue("spcontato", Contato);
+            cmd.Parameters.AddWithValue("sptelefone", Telefone);
+            cmd.Parameters.AddWithValue("spemail", Email);
+            cmd.ExecuteNonQuery();
+        }
+        public static Fornecedor ObterPorID(int Id)
+        {
+            Fornecedor fornecedor = new();
+            var cmd = Banco.Abrir();
+            cmd.CommandText = $"Select * from fornecedores where id = {Id}";
+            var dr = cmd.ExecuteReader();
+           /* while (dr.Read())
+            {
+                fornecedor = new(
+                    dr.GetInt32(0),
+                    dr.GetString(1),
+                    );
+            }*/
+            return fornecedor;
+        }
+        public static List<Fornecedor> ObterPorLista()
+        {
+            List<Fornecedor> lista = new();
+            return lista;
+        }
+
     }
 }
--- SysPecNsDesk/FrmFornecedores.Designer.cs
+++ SysPecNsDesk/FrmFornecedores.Designer.cs
@@ -28,19 +28,59 @@
         /// </summary>
         private void InitializeComponent()
         {
+            TabCtFornecedores = new TabControl();
+            CadFornecedores = new TabPage();
+            ListaFornecedores = new TabPage();
+            TabCtFornecedores.SuspendLayout();
             SuspendLayout();
+            // 
+            // TabCtFornecedores
+            // 
+            TabCtFornecedores.Controls.Add(CadFornecedores);
+            TabCtFornecedores.Controls.Add(ListaFornecedores);
+            TabCtFornecedores.Location = new Point(88, 42);
+            TabCtFornecedores.Name = "TabCtFornecedores";
+            TabCtFornecedores.SelectedIndex = 0;
+            TabCtFornecedores.Size = new Size(574, 358);
+            TabCtFornecedores.TabIndex = 0;
+            // 
+            // CadFornecedores
+            // 
+            CadFornecedores.Location = new Point(4, 24);
+            CadFornecedores.Name = "CadFornecedores";
+            CadFornecedores.Padding = new Padding(3);
+            CadFornecedores.Size = new Size(566, 330);
+            CadFornecedores.TabIndex = 0;
+            CadFornecedores.Text = "Cadastro de Fornecedores";
+            CadFornecedores.UseVisualStyleBackColor = true;
+            // 
+            // ListaFornecedores
+            // 
+            ListaFornecedores.Location = new Point(4, 24);
+            ListaFornecedores.Name = "ListaFornecedores";
+            ListaFornecedores.Padding = new Padding(3);
+            ListaFornecedores.Size = new Size(566, 330);
+            ListaFornecedores.TabIndex = 1;
+            ListaFornecedores.Text = "Lista de Fornecedores";
+            ListaFornecedores.UseVisualStyleBackColor = true;
             // 
             // FrmFornecedores
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(TabCtFornecedores);
             Name = "FrmFornecedores";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "FrmFornecedores";
+            TabCtFornecedores.ResumeLayout(false);
             ResumeLayout(false);
         }
 
         #endregion
+
+        private TabControl TabCtFornecedores;
+        private TabPage CadFornecedores;
+        private TabPage ListaFornecedores;
     }
 }
